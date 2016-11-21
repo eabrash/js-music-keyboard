ee5443da1a727a37620390b396cdbe5550810c6d
Emily Abrash <emabrash@gmail.com>
2016-11-21 14:46:27 -0800

    Minor edits to comments

--- noise.js
+++ noise.js
@@ -4,7 +4,7 @@
 $(document).ready( function() {
 
   // Play a note when the button is clicked. Assumes that the HTML stays as it
-  // is, so that the note (a, b, c, etc.) is the second class of the element, and
+  // is, so that the note (a, b, c, etc.) is the second class ([1]) of the element, and
   // that other elements, such as #aAudio, stay named as they are presently.
 
   $('.instrument').on('click', '.note', (function(event){
@@ -17,12 +17,14 @@ $(document).ready( function() {
   // Play a note when a keyboard key is pressed. Makes similar assumptions to above
   // about class naming, though it does not depend on the order of the classes on the
   // note button elements.
+  // Regex reference: http://stackoverflow.com/questions/7070975/regex-one-character-only
 
   $('body').keydown(function(event){
     if (/^[a-g]$/.test(event.key)){
       $('#' + event.key + 'Audio')[0].load();
       $('#' + event.key + 'Audio')[0].play();
       $('.' + event.key).addClass('active');
+      // Makes the note button flash as if clicked
       setTimeout(function(){
         $('.' + event.key).removeClass('active');
       }, 80);
